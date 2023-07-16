ccae162cefa40a63d28e7ad100027eed31b4c3f6
Anup <86971824+Lilnxxx@users.noreply.github.com>
2023-07-16 11:28:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app= express()
 
 app.use(express.json())
 app.use('/api',apiR)
-app.listen(process.env.port||5000,()=>{
+app.listen(process.env.PORT||5000,()=>{
     console.log("ready on port 5000")
 })
